f94ec1704120b3ebf4d88d8ec3abf4b92d240aec
aman22-07 <aman12729921@gmail.com>
2025-10-23 19:57:54 +0530

    Update Basic_Oops.java

--- src/main/java/OOPs/Basic_Oops.java
+++ src/main/java/OOPs/Basic_Oops.java
@@ -1,1 +1,35 @@
+// Define a class named Animal8
+class Animal8 
+{
+    // Method to display a running message
+    void run() 
+    {
+        System.out.println("I'm running");
+    }
+}
 
+// Define a class named Birds8
+class Birds8 
+{
+    // Method to display a flying message
+    void fly() 
+    {
+        System.out.println("I'm flying");
+    }
+}
+
+// Define the main class MainApp8
+public class Basic_Oops
+{
+    // Main method - program entry point
+    public static void main(String[] args) 
+    {
+        // Create an object 'buzo' of Animal8 and call the run method
+        Animal8 buzo = new Animal8();
+        buzo.run();
+
+        // Create an object 'sparrow' of Birds8 and call the fly method
+        Birds8 sparrow = new Birds8();
+        sparrow.fly();
+    }
+}
